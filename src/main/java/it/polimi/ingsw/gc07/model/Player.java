83d0811d1665fcf413045b37c78bfab25208f25c
kevinziroldi <k.ziroldi@gmail.com>
2024-03-08 10:38:36 +0100

    Adding documentation and TODOs

--- src/main/java/it/polimi/ingsw/gc07/model/Player.java
+++ src/main/java/it/polimi/ingsw/gc07/model/Player.java
@@ -3,15 +3,53 @@ package it.polimi.ingsw.gc07.model;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Class representing a player.
+ */
 public class Player {
+    /**
+     * Player's nickname.
+     */
     private String nickname;
+    /**
+     * Color of the player's token.
+     */
     private TokenColor tokenColor;
+    /**
+     * Boolean value representing if the player is the first one.
+     * true: the player is first
+     * false: the player is not first
+     */
     private boolean isFirst;
+    /**
+     * List of cards the player currently has.
+     */
     private List<NonStarterCard> currentHand;
+    /**
+     * Player's game field.
+     */
     private GameField gameField;
+    /**
+     * Player's secret objective, it is an objective card.
+     */
     private ObjectiveCard secretObjective;
+    /**
+     * Player's connection type.
+     * true: RMI
+     * false: socket
+     */
     private boolean connectionType;
 
+    /**
+     * Constructor of class player
+     * @param nickname player's nickname
+     * @param tokenColor player's token color
+     * @param isFirst true for first player
+     * @param currentHand current hand
+     * @param gameField player's game field
+     * @param secretObjective player's secret objective card
+     * @param connectionType player's connection type
+     */
     // TODO: prendo riferimento a GameField
     public Player(String nickname, TokenColor tokenColor, boolean isFirst,
                   List<NonStarterCard> currentHand, GameField gameField,
@@ -25,25 +63,63 @@ public class Player {
         this.connectionType = connectionType;
     }
 
+    /**
+     * Getter for nickname.
+     * @return this.nickname
+     */
     public String getNickname() {
         return nickname;
     }
+
+    /**
+     * Getter for token color.
+     * @return token color
+     */
     public TokenColor getTokenColor() {
         return tokenColor;
     }
+
+    /**
+     * Getter for attribute isFirst
+     * @return true if the player is first
+     */
     public boolean isFirst() {
         return isFirst;
     }
+
+    /**
+     * Setter for cards in player's hand.
+     * @param currentHand current card in player's hand
+     */
     public void setCurrentHand(List<NonStarterCard> currentHand){}
+
+    /**
+     * Getter for cards in player's hand.
+     * @return currentHand current card in player's hand
+     */
     public List<NonStarterCard> getCurrentHand() {
         return new ArrayList<>(currentHand);
     }
+
+    /**
+     * Getter for game field.
+     * @return game field
+     */
     // TODO: sfugge riferimento a GameField
     public GameField getGameField() {
         return gameField;
     }
+
+    /**
+     * Getter for the secret objective card.
+     * @return secret objective card
+     */
     public ObjectiveCard getSecretObjective() {
         return secretObjective;
     }
+    /**
+     * Getter for the connection type
+     * @return connection type
+     */
     public boolean getConnectionType(){}
 }
--- src/main/java/it/polimi/ingsw/gc07/model/TokenColor.java
+++ src/main/java/it/polimi/ingsw/gc07/model/TokenColor.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.gc07.model;
 
+/**
+ * Enum class representing token colors in the game.
+ */
 public enum TokenColor {
     BLUE, GREEN, RED, YELLOW;
 }
