ac5dc5e80288016efa580fb46c442f83d1d4151b
kevinziroldi <k.ziroldi@gmail.com>
2024-03-12 10:57:12 +0100

    Added method and exception to chat

--- src/main/java/it/polimi/ingsw/gc07/model/Chat.java
+++ src/main/java/it/polimi/ingsw/gc07/model/Chat.java
@@ -1,7 +1,10 @@
 package it.polimi.ingsw.gc07.model;
 
 
+import it.polimi.ingsw.gc07.exceptions.EmptyChatException;
+
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 
 public class Chat {
@@ -15,8 +18,17 @@ public class Chat {
         this.content.add(newMessage);
     }
 
+    public String getLastMessage() throws EmptyChatException {
+        if(content.size() < 1){
+            throw new EmptyChatException();
+        }
+        return content.get(content.size() - 1);
+    }
+
     public List<String> getContent() {
         return new ArrayList<>(content);
     }
+
+    // Poi dovremo capire come
 }
 
