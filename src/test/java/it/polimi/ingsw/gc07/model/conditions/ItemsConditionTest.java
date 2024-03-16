410b07d1f981a107f9423dc208ba7372ba7937f1
kevinziroldi <k.ziroldi@gmail.com>
2024-03-16 17:46:49 +0100

    Created CornerCoverageConditionTest and ItemsConditionTest

---
+++ src/test/java/it/polimi/ingsw/gc07/model/conditions/ItemsConditionTest.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.gc07.model.conditions;
+
+import it.polimi.ingsw.gc07.model.GameField;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemsConditionTest {
+    GameField gameField;
+    Condition condition;
+
+    public void setUp() {
+        // TODO
+        // create a gameField, containing game items
+        // create condition
+    }
+
+    @Test
+    public void testNumTimesMet(){
+        // TODO
+    }
+}
