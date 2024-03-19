e4a3bc4794bb71ff35929e0ff740ca6cf95b5f92
Tommaso0209 <tommaso.uberti@mail.polimi.it>
2024-03-19 17:04:10 +0100

    added checkAvailability, modified PlacementResult, modified placeCard in GameField

--- src/main/java/it/polimi/ingsw/gc07/exceptions/PlacementResult.java
+++ src/main/java/it/polimi/ingsw/gc07/exceptions/PlacementResult.java
@@ -6,7 +6,6 @@ package it.polimi.ingsw.gc07.exceptions;
  * NO_COVERED_CORNER: the card doesn't cover any corner
  * NOT_LEGIT_CORNER: the card covers a non-corner
  * MULTIPLE_CORNERS_COVERED: the card covers multiple corners of the same card
- * PLACING_CONDITION_NOT_MET: the gold can't be placed because the placing condition is not met
  * CARD_ALREADY_PRESENT: there is a card in the specified indexes
  * INDEXES_OUT_OF_GAME_FIELD: specified indexes exceed the game field size
  *
@@ -16,7 +15,6 @@ public enum PlacementResult {
     NO_COVERED_CORNER, // NoCoveredCornerException
     NOT_LEGIT_CORNER, // NotLegitCornerException
     MULTIPLE_CORNERS_COVERED, // MultipleCornersCoveredException
-    PLACING_CONDITION_NOT_MET, //  PlacingConditionNotMetException
-    CARD_ALREADY_PRESENT, // CardAlreadyPresentExcpetion
+    CARD_ALREADY_PRESENT, // CardAlreadyPresentException
     INDEXES_OUT_OF_GAME_FIELD // IndexesOutOfGameFieldException
 }
