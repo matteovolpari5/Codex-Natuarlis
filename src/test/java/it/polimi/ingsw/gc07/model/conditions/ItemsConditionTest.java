a31036e1cef94debc5a72ac013959d7ff6999053
kevinziroldi <k.ziroldi@gmail.com>
2024-03-25 08:19:48 +0100

    Started ItemsConditionTest

--- src/test/java/it/polimi/ingsw/gc07/model/conditions/ItemsConditionTest.java
+++ src/test/java/it/polimi/ingsw/gc07/model/conditions/ItemsConditionTest.java
@@ -1,21 +1,30 @@
 package it.polimi.ingsw.gc07.model.conditions;
 
+import it.polimi.ingsw.gc07.controller.Game;
 import it.polimi.ingsw.gc07.model.GameField;
+import it.polimi.ingsw.gc07.model.GameItem;
+import it.polimi.ingsw.gc07.model.enumerations.GameObject;
+import it.polimi.ingsw.gc07.model.enumerations.GameResource;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemsConditionTest {
-    GameField gameField;
-    Condition condition;
-
-    public void setUp() {
-        // TODO
-        // create a gameField, containing game items
-        // create condition
-    }
 
     @Test
-    public void testNumTimesMet(){
-        // TODO
+    public void constructorAndGetter() {
+        List<GameItem> myList = new ArrayList<>();
+        myList.add(GameObject.QUILL);
+        myList.add(GameObject.INKWELL);
+        myList.add(GameResource.ANIMAL);
+        myList.add(GameResource.FUNGI);
+        ItemsCondition condition = new ItemsCondition(myList);
+        List<GameItem> checkList = condition.getNeededItems();
+        assertNotNull(checkList);
+        assertEquals(myList, checkList);
     }
+
 }
