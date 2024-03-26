7f53473ae5770f7d51bb8f61d92c5d147392b192
kevinziroldi <k.ziroldi@gmail.com>
2024-03-26 20:27:24 +0100

    Started testing Player

--- src/main/java/it/polimi/ingsw/gc07/model/Player.java
+++ src/main/java/it/polimi/ingsw/gc07/model/Player.java
@@ -2,10 +2,8 @@ package it.polimi.ingsw.gc07.model;
 
 import it.polimi.ingsw.gc07.model.cards.DrawableCard;
 import it.polimi.ingsw.gc07.model.cards.ObjectiveCard;
-import it.polimi.ingsw.gc07.model.cards.PlaceableCard;
 import it.polimi.ingsw.gc07.model.enumerations.TokenColor;
 
-import java.util.Currency;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -16,11 +14,11 @@ public class Player {
     /**
      * Player's nickname.
      */
-    private String nickname;
+    private final String nickname;
     /**
      * Color of the player's token.
      */
-    private TokenColor tokenColor;
+    private final TokenColor tokenColor;
     /**
      * Boolean value representing if the player is first.
      */
@@ -30,13 +28,13 @@ public class Player {
      * true: RMI
      * false: socket
      */
-    private boolean connectionType;
+    private final boolean connectionType;
     /**
      * Player's connection type.
      * true: GUI
      * false: TUI
      */
-    private boolean interfaceType;
+    private final boolean interfaceType;
     /**
      * Boolean value representing if the player is connected.
      */
@@ -64,9 +62,9 @@ public class Player {
         this.nickname = nickname;
         this.tokenColor = tokenColor;
         this.isFirst = false;   // will be set true only for the first player
-        this.isConnected = true;
         this.connectionType = connectionType;
         this.interfaceType = interfaceType;
+        this.isConnected = true;
         this.currentHand = new ArrayList<>();
         this.secretObjective = null;
         this.isStalled = false;
@@ -80,9 +78,9 @@ public class Player {
         this.nickname = existingPlayer.nickname;
         this.tokenColor = existingPlayer.tokenColor;
         this.isFirst = existingPlayer.isFirst;
-        this.isConnected = existingPlayer.isConnected;
         this.connectionType = existingPlayer.connectionType;
         this.interfaceType = existingPlayer.interfaceType;
+        this.isConnected = existingPlayer.isConnected;
         this.currentHand = new ArrayList<>(existingPlayer.currentHand);
         this.secretObjective = existingPlayer.secretObjective;
         this.isStalled = existingPlayer.isStalled;
@@ -105,10 +103,34 @@ public class Player {
     }
 
     /**
-     * setter for attribute isFirst
+     * Setter for attribute isFirst.
      */
     public void setFirst(){
         this.isFirst = true;
+    }
+
+    /**
+     * Getter for attribute isFirst.
+     * @return true if the player is first
+     */
+    public boolean isFirst() {
+        return isFirst;
+    }
+
+    /**
+     * Getter for the connection type.
+     * @return true = RMI, false = SOCKET
+     */
+    public boolean getConnectionType(){
+        return this.connectionType;
+    }
+
+    /**
+     * Getter method for the interface type.
+     * @return true = GUI, false = TUI
+     */
+    public boolean getInterfaceType(){
+        return this.interfaceType;
     }
 
     /**
@@ -125,14 +147,6 @@ public class Player {
     public void setIsStalled(boolean isStalled)
     {
         this.isStalled=isStalled;
-    }
-
-    /**
-     * Getter for attribute isFirst
-     * @return true if the player is first
-     */
-    public boolean isFirst() {
-        return isFirst;
     }
 
     /**
@@ -187,17 +201,5 @@ public class Player {
     public ObjectiveCard getSecretObjective() {
         // card is immutable, I can return it
         return secretObjective;
-    }
-
-    /**
-     * Getter for the connection type
-     * @return connection type
-     */
-    public boolean getConnectionType(){
-        return this.connectionType;
-    }
-
-    public boolean getInterfaceType(){
-        return this.interfaceType;
     }
 }
---
+++ src/test/java/it/polimi/ingsw/gc07/model/PlayerTest.java
@@ -0,0 +1,81 @@
+package it.polimi.ingsw.gc07.model;
+
+import it.polimi.ingsw.gc07.controller.DecksBuilder;
+import it.polimi.ingsw.gc07.exceptions.CardNotPresentException;
+import it.polimi.ingsw.gc07.model.cards.DrawableCard;
+import it.polimi.ingsw.gc07.model.decks.ResourceCardsDeck;
+import it.polimi.ingsw.gc07.model.enumerations.TokenColor;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    public void checkInitAttributes() {
+        // create a test player
+        String nickname = "TestNickname";
+        TokenColor tokenColor = TokenColor.RED;
+        boolean connectionType = true;
+        boolean interfaceType = true;
+        Player player = new Player(nickname, tokenColor, connectionType, interfaceType);
+        assertEquals(nickname, player.getNickname());
+        assertEquals(tokenColor, player.getTokenColor());
+        assertFalse(player.isFirst());
+        assertEquals(connectionType, player.getConnectionType());
+        assertEquals(interfaceType, player.getInterfaceType());
+        assertTrue(player.isConnected());
+        assertFalse(player.getIsStalled());
+        assertNull(player.getSecretObjective());
+        assertNotNull(player.getCurrentHand());
+        assertEquals(0, player.getCurrentHand().size());
+    }
+
+    @Test
+    public void checkAddCardHand() {
+        ResourceCardsDeck reosurceCardsDeck = DecksBuilder.buildResourceCardsDeck();
+        reosurceCardsDeck.shuffle();
+
+        // create a test player
+        String nickname = "TestNickname";
+        TokenColor tokenColor = TokenColor.RED;
+        boolean connectionType = true;
+        boolean interfaceType = true;
+        Player player = new Player(nickname, tokenColor, connectionType, interfaceType);
+
+        assertEquals(0, player.getCurrentHand().size());
+        try {
+            player.addCardHand(reosurceCardsDeck.drawCard());
+        } catch (CardNotPresentException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(1, player.getCurrentHand().size());
+        try {
+            player.addCardHand(reosurceCardsDeck.drawCard());
+        } catch (CardNotPresentException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(2, player.getCurrentHand().size());
+        List<DrawableCard> currentHand = player.getCurrentHand();
+        DrawableCard card = null;
+        try {
+            card = reosurceCardsDeck.drawCard();
+            player.addCardHand(card);
+        } catch (CardNotPresentException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(3, player.getCurrentHand().size());
+        List<DrawableCard> newCurrentHand = player.getCurrentHand();
+        for(DrawableCard c: currentHand){
+            assertTrue(newCurrentHand.contains(c));
+        }
+        assertTrue(newCurrentHand.contains(card));
+    }
+
+    @Test
+    public void removeAddCardHand() {
+
+    }
+}
