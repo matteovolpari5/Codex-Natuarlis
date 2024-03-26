4b5a5e9ed59a430479658c9b84e1dd1e763a2681
kevinziroldi <k.ziroldi@gmail.com>
2024-03-26 18:41:09 +0100

    Updates to JavaDoc

--- src/main/java/it/polimi/ingsw/gc07/model/enumerations/CardType.java
+++ src/main/java/it/polimi/ingsw/gc07/model/enumerations/CardType.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.gc07.model.enumerations;
 
 /**
- * Enum class representing card types
+ * Enum class representing card types.
  */
 public enum CardType {
     RESOURCE_CARD, GOLD_CARD, STARTER_CARD, OBJECTIVE_CARD;
