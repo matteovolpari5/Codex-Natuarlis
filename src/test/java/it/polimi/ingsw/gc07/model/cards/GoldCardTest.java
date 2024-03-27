1fc6c73fcda918a43a6eca7ac6235e5f0c022826
matteovolpari <matteo.volpari@gmail.com>
2024-03-27 22:52:58 +0100

    finished isPlaceable testing for GoldCardTest

--- src/test/java/it/polimi/ingsw/gc07/model/cards/GoldCardTest.java
+++ src/test/java/it/polimi/ingsw/gc07/model/cards/GoldCardTest.java
@@ -26,7 +26,189 @@ class GoldCardTest {
     }
 
     @Test
-    void isPlaceable() {
+    void isGoldPlaceableNoLegitCorner() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException {
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 49){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 2){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, false);
+            }
+            if (c.getId() == 3){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, false);
+            }
+            if (c.getId() == 1){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, false);
+            }
+            if (c.getId() == 4){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, false);
+            }
+        }
+        assertEquals(PlacementResult.NOT_LEGIT_CORNER, myGoldCard.isPlaceable(gameField, 42, 40, false));
+    }
+
+    @Test
+    void isGoldPlaceableNoCoveredCorner() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException{
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 58){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 2){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, true);
+            }
+            if (c.getId() == 3){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, true);
+            }
+            if (c.getId() == 1){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, true);
+            }
+            if (c.getId() == 4){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, true);
+            }
+        }
+        assertEquals(PlacementResult.NO_COVERED_CORNER, myGoldCard.isPlaceable(gameField, 45, 45, true));
+    }
+
+    @Test
+    void isGoldPlaceableMultipleCorner() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException{
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 58){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 2){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, true);
+            }
+            if (c.getId() == 3){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, true);
+            }
+            if (c.getId() == 1){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, true);
+            }
+            if (c.getId() == 4){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, true);
+            }
+        }
+        assertEquals(PlacementResult.MULTIPLE_CORNERS_COVERED, myGoldCard.isPlaceable(gameField, 42, 41, true));
+    }
+
+    @Test
+    void isGoldPlaceableIndexOut() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException{
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 58){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 2){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, true);
+            }
+            if (c.getId() == 3){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, true);
+            }
+            if (c.getId() == 1){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, true);
+            }
+            if (c.getId() == 4){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, true);
+            }
+        }
+        assertEquals(PlacementResult.INDEXES_OUT_OF_GAME_FIELD, myGoldCard.isPlaceable(gameField, 89, 10, true));
+    }
+
+    @Test
+    void isGoldPlaceableCardAlreadyPresent() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException{
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 58){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 2){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, true);
+            }
+            if (c.getId() == 3){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, true);
+            }
+            if (c.getId() == 1){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, true);
+            }
+            if (c.getId() == 4){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, true);
+            }
+        }
+        assertEquals(PlacementResult.CARD_ALREADY_PRESENT, myGoldCard.isPlaceable(gameField, 41, 41, true));
+    }
+    @Test
+    void isGoldPlaceableCondition() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException {
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 58){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 2){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, true);
+            }
+            if (c.getId() == 3){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, true);
+            }
+            if (c.getId() == 1){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, true);
+            }
+            if (c.getId() == 4){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, true);
+            }
+        }
+        assertEquals(PlacementResult.PLACING_CONDITION_NOT_MET, myGoldCard.isPlaceable(gameField, 42, 40, false));
+
     }
 
     @Test
@@ -91,5 +273,37 @@ class GoldCardTest {
         }
         gameField.placeCard(myGoldCard, 38, 40, false);
         assertEquals(4, myGoldCard.getPlacementScore(gameField, 38, 40));
+    }
+    @Test
+    void getGoldItemsPlacementScore() throws CardAlreadyPresentException, IndexesOutOfGameFieldException, PlacingConditionNotMetException, MultipleCornersCoveredException, NotLegitCornerException, NoCoveredCornerException, CardNotPresentException {
+        myStarterCard = starterCardsDeck.drawCard();
+        GameField gameField = new GameField(myStarterCard);
+        gameField.placeCard(myStarterCard, 40, 40, false);
+        for(DrawableCard c: goldCardsDeck.getContent()){
+            if(c.getId() == 42){
+                myGoldCard = c;
+            }
+        }
+        for(DrawableCard c: resourceCardsDeck.getContent()){
+            if (c.getId() == 37){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 39, false);
+            }
+            if (c.getId() == 6){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 39, false);
+            }
+            if (c.getId() == 16){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 39, 41, false);
+            }
+            if (c.getId() == 25){
+                myResourceCard = c;
+                gameField.placeCard(myResourceCard, 41, 41, false);
+            }
+        }
+        assertEquals(PlacementResult.SUCCESS, myGoldCard.isPlaceable(gameField, 42, 42, false));
+        gameField.placeCard(myGoldCard, 42, 42, false);
+        assertEquals(5, myGoldCard.getPlacementScore(gameField, 42, 42));
     }
 }
