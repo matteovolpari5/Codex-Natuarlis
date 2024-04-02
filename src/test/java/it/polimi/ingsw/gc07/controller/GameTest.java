665a104f2312c3488c9365842b9d7a4989257b99
kevinziroldi <k.ziroldi@gmail.com>
2024-04-02 16:53:32 +0200

    Tested Game constructor

--- src/test/java/it/polimi/ingsw/gc07/controller/GameTest.java
+++ src/test/java/it/polimi/ingsw/gc07/controller/GameTest.java
@@ -1,7 +1,41 @@
 package it.polimi.ingsw.gc07.controller;
 
+import it.polimi.ingsw.gc07.DecksBuilder;
+import it.polimi.ingsw.gc07.exceptions.WrongNumberOfPlayersException;
+import it.polimi.ingsw.gc07.model.cards.ObjectiveCard;
+import it.polimi.ingsw.gc07.model.cards.PlaceableCard;
+import it.polimi.ingsw.gc07.model.decks.Deck;
+import it.polimi.ingsw.gc07.model.decks.GoldCardsDeck;
+import it.polimi.ingsw.gc07.model.decks.PlayingDeck;
+import it.polimi.ingsw.gc07.model.decks.ResourceCardsDeck;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
-
+    @Test
+    void checkWrongPlayersNumber() {
+        int id = 0;
+        ResourceCardsDeck resourceCardsDeck = DecksBuilder.buildResourceCardsDeck();
+        resourceCardsDeck.shuffle();
+        GoldCardsDeck goldCardsDeck = DecksBuilder.buildGoldCardsDeck();
+        goldCardsDeck.shuffle();
+        PlayingDeck<ObjectiveCard> objectiveCardsDeck = DecksBuilder.buildObjectiveCardsDeck();
+        objectiveCardsDeck.shuffle();
+        Deck<PlaceableCard> starterCardsDeck = DecksBuilder.buildStarterCardsDeck();
+        starterCardsDeck.shuffle();
+        assertThrows(WrongNumberOfPlayersException.class,
+                () -> new Game(id, 0, resourceCardsDeck,
+                goldCardsDeck, objectiveCardsDeck, starterCardsDeck));
+        assertThrows(WrongNumberOfPlayersException.class,
+                () -> new Game(id, 1, resourceCardsDeck,
+                        goldCardsDeck, objectiveCardsDeck, starterCardsDeck));
+        assertDoesNotThrow(() -> new Game(id, 2, resourceCardsDeck,
+                goldCardsDeck, objectiveCardsDeck, starterCardsDeck));
+        assertDoesNotThrow(() -> new Game(id, 4, resourceCardsDeck,
+                goldCardsDeck, objectiveCardsDeck, starterCardsDeck));
+        assertThrows(WrongNumberOfPlayersException.class,
+                () -> new Game(id, 5, resourceCardsDeck,
+                        goldCardsDeck, objectiveCardsDeck, starterCardsDeck));
+    }
 }
