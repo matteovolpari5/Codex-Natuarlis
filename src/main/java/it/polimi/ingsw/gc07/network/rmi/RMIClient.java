c3c7c51633a475b5eac58c177be0bdba434196af
kevinziroldi <k.ziroldi@gmail.com>
2024-04-05 19:10:50 +0200

    Started implementing RMI

--- src/main/java/it/polimi/ingsw/gc07/network/rmi/RMIClient.java
+++ src/main/java/it/polimi/ingsw/gc07/network/rmi/RMIClient.java
@@ -1,15 +1,48 @@
 package it.polimi.ingsw.gc07.network.rmi;
 
-import it.polimi.ingsw.gc07.exceptions.PlayerAlreadyPresentException;
-import it.polimi.ingsw.gc07.model.enumerations.TokenColor;
-
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 public class RMIClient extends UnicastRemoteObject implements VirtualView {
     final VirtualServer server;
 
     protected RMIClient(VirtualServer server) throws RemoteException {
         this.server = server;
+    }
+
+    private void run() {
+        try {
+            server.connect(this);
+            runCli();
+        }catch(RemoteException e) {
+            //TODO
+        }
+    }
+
+    private void runCli() {
+        Scanner scan = new Scanner(System.in);
+        while(true) {
+            System.out.print("> ");
+            String command = scan.nextLine();
+            if(command != null) {
+                System.out.println("Command received: " + command);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.getRegistry(args[0], 1234);
+            VirtualServer server = (VirtualServer) registry.lookup("VirtualServer");
+            new RMIClient(server).run();
+        }catch(RemoteException e) {
+            //TODO manage remote exception
+        }catch(NotBoundException e) {
+            //TODO manage not bound exception
+        }
     }
 }
--- src/main/java/it/polimi/ingsw/gc07/network/rmi/VirtualServer.java
+++ src/main/java/it/polimi/ingsw/gc07/network/rmi/VirtualServer.java
@@ -1,10 +1,22 @@
 package it.polimi.ingsw.gc07.network.rmi;
 
-import it.polimi.ingsw.gc07.exceptions.PlayerAlreadyPresentException;
-import it.polimi.ingsw.gc07.model.enumerations.TokenColor;
+import it.polimi.ingsw.gc07.controller.GameCommand;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface VirtualServer extends Remote {
+    /**
+     * Method to connect a new client.
+     * @param client client to connect
+     * @throws RemoteException remote exception
+     */
+    void connect(VirtualView client) throws RemoteException;
+
+    /**
+     * Method to set a command and execute it.
+     * @param gameCommand game command to set and execute
+     * @throws RemoteException remote exception
+     */
+    void setAndExecuteCommand(GameCommand gameCommand) throws RemoteException;
 }
