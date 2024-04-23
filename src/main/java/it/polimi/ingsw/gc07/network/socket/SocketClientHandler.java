8025c0cc4a9724987740f110c3b0c0a612231e5e
Tommaso0209 <tommaso.uberti@mail.polimi.it>
2024-04-23 14:37:34 +0200

    Third update on Socket

--- src/main/java/it/polimi/ingsw/gc07/network/socket/SocketClientHandler.java
+++ src/main/java/it/polimi/ingsw/gc07/network/socket/SocketClientHandler.java
@@ -16,25 +16,25 @@ import java.rmi.RemoteException;
 public class SocketClientHandler implements VirtualView {
     private final GamesManager gamesManager;
     private Game game;
-    private final SocketGamesManagerServer GamesManagerserver;
     private final Socket mySocket;
+    private final ObjectInputStream input;
+    private final ObjectOutputStream output;
 
-    public SocketClientHandler(GamesManager gamesManager, SocketGamesManagerServer server, Socket mySocket){
+    public SocketClientHandler(GamesManager gamesManager, Socket mySocket) throws IOException {
         this.gamesManager = gamesManager;
-        this.GamesManagerserver = server;
-        this.mySocket = mySocket;
         this.game = null;
+        this.mySocket = mySocket;
+        this.input = new ObjectInputStream(mySocket.getInputStream());
+        this.output = new ObjectOutputStream(mySocket.getOutputStream());
+        new Thread(this::manageGamesManagerCommand).start();
     }
 
     public void manageGamesManagerCommand(){
 
         GamesManagerCommand command;
-        ObjectInputStream input = null;
-        ObjectOutputStream output = null;
+
         while(true){
             try{
-                input = new ObjectInputStream(mySocket.getInputStream());
-                output = new ObjectOutputStream(mySocket.getOutputStream());
                 command = (GamesManagerCommand) input.readObject();
                 synchronized (gamesManager){
                     gamesManager.setAndExecuteCommand(command);
@@ -50,14 +50,12 @@ public class SocketClientHandler implements VirtualView {
                             throw new RuntimeException();
                         }
                         this.game = gamesManager.getGameById(gameId);
-                        int port = GamesManagerserver.manageConnectionToGame(result, gameId);
-                        Socket socketClient = new Socket("host", port);
-                        //TODO decidere come gestire la sostituzione dell'input e dell'output, per ora sostituiti direttamente da ora in avanti
-                        input = new ObjectInputStream(socketClient.getInputStream());
-                        output = new ObjectOutputStream(socketClient.getOutputStream());
+
+                        //TODO output.write->.reset()->.flush()
                         output.writeBytes("Benvenuto"); //TODO oppure .writeChars(...);
+
                         output.flush();
-                        output.reset(); //TODO necessario anche se non è un oggetto?
+                         //TODO necessario anche se non è un oggetto?
                         break;
                     }
                 }
@@ -69,12 +67,10 @@ public class SocketClientHandler implements VirtualView {
             }
         }
         manageGameCommand();
-        //oppure alla riga 55 non si crea un'altro socket ma si riusa mySocket, in tal caso bisgona capire come gestire la chiusura del socket, input e output
     }
 
     private void manageGameCommand(){
-        //TODO completare
-        closeConnection(mySocket, input, output); //forse realizzare come alla riga 73 così mySocket è il socket con Game, comunque sia deve essere cambiato mySocket
+        closeConnection(mySocket, input, output);
     }
 
     public void closeConnection(Socket mySocket, ObjectInputStream input, ObjectOutputStream output){
