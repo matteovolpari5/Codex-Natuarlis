e54212ad2c8818304fe63a4f73025bc9210b2d82
SamueleVilla02 <samuele1.villa@mail.polimi.it>
2024-04-23 10:21:18 +0200

    add getter and setter for class GameFieldView
    @SV

--- src/main/java/it/polimi/ingsw/gc07/model_view/GameFieldView.java
+++ src/main/java/it/polimi/ingsw/gc07/model_view/GameFieldView.java
@@ -1,7 +1,4 @@
 package it.polimi.ingsw.gc07.model_view;
-
-import it.polimi.ingsw.gc07.model.CommandResult;
-import it.polimi.ingsw.gc07.model.GameField;
 import it.polimi.ingsw.gc07.model.cards.PlaceableCard;
 
 public class GameFieldView {
@@ -9,6 +6,7 @@ public class GameFieldView {
      * Player's starter card.
      */
     private PlaceableCard starterCard;
+
     /**
      * Each cell of the matrix contains a placeable card, or null if the place is empty.
      */
@@ -39,6 +37,22 @@ public class GameFieldView {
     }
 
     /**
+     * Getter method for the game field content.
+     */
+    public PlaceableCard[][] getCardsContent() {return cardsContent;}
+
+    /**
+     * Setter method for the game field content.
+     * @param cardsContent new game field
+     */
+    public void setCardsContent(PlaceableCard[][] cardsContent) {this.cardsContent = cardsContent;}
+
+    /**
+     * Getter method for the starter card.
+     */
+    public PlaceableCard getStarterCard() {return starterCard;}
+
+    /**
      * Setter method for the starter card.
      * @param starterCard starter card
      */
@@ -46,7 +60,27 @@ public class GameFieldView {
         this.starterCard = starterCard;
     }
 
-    //TODO
-    // in realtà ogni volta riassegno l'intero game field
-    // penso servano solo getter / robe per stampare
+    /**
+     * Getter method for the card's face.
+     */
+    public Boolean[][] getCardsFace() {return cardsFace;}
+
+    /**
+     * Setter method for the card's face.
+     * @param cardsFace new matrix of card's face.
+     */
+    public void setCardsFace(Boolean[][] cardsFace) {this.cardsFace = cardsFace;}
+
+    /**
+     * Getter method for the card's order.
+     */
+    public int[][] getCardsOrder() {return cardsOrder;}
+
+    /**
+     * Setter method for the card's order.
+     * @param cardsOrder new matrix of card's order.
+     */
+    public void setCardsOrder(int[][] cardsOrder) {this.cardsOrder = cardsOrder;}
+
+    //TODO: robe per stampare
 }
