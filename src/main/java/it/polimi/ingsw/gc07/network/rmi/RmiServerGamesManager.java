da3a6ab9343f52a0621c7e1a8ef732792c9f4557
kevinziroldi <k.ziroldi@gmail.com>
2024-04-29 21:27:52 +0200

    Made RmiServerGame asynchronous

--- src/main/java/it/polimi/ingsw/gc07/network/rmi/RmiServerGamesManager.java
+++ src/main/java/it/polimi/ingsw/gc07/network/rmi/RmiServerGamesManager.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw.gc07.network.rmi;
 
-import it.polimi.ingsw.gc07.controller.GameController;
 import it.polimi.ingsw.gc07.controller.GamesManager;
-import it.polimi.ingsw.gc07.game_commands.DisplayGamesCommand;
 import it.polimi.ingsw.gc07.game_commands.GamesManagerCommand;
 import it.polimi.ingsw.gc07.model.enumerations.CommandResult;
 import it.polimi.ingsw.gc07.network.VirtualServerGamesManager;
