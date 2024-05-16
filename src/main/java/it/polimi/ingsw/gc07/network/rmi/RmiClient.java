900fc966efabfa84e6998774cb15ed4be0fa1a99
kevinziroldi <k.ziroldi@gmail.com>
2024-05-16 10:08:58 +0200

    fix clients

--- src/main/java/it/polimi/ingsw/gc07/network/rmi/RmiClient.java
+++ src/main/java/it/polimi/ingsw/gc07/network/rmi/RmiClient.java
@@ -309,4 +309,9 @@ public class RmiClient extends UnicastRemoteObject implements Client, VirtualVie
     public void receiveDeckUpdate(DeckUpdate deckUpdate) throws RemoteException {
         deckUpdate.execute(gameView);
     }
+
+    @Override
+    public void receiveGameEndedUpdate(GameEndedUpdate gameEndedUpdate) throws RemoteException {
+        gameEndedUpdate.execute(gameView);
+    }
 }
