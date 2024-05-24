6407f9c43119f4f84c6f15e03cba3aee28a376b3
kevinziroldi <k.ziroldi@gmail.com>
2024-05-24 18:18:16 +0200

    fix

--- src/main/java/it/polimi/ingsw/gc07/view/gui/SceneType.java
+++ src/main/java/it/polimi/ingsw/gc07/view/gui/SceneType.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.gc07.view.gui;
 
 public enum SceneType {
     LOBBY_SCENE("Codex Naturalis - Lobby", "/it/polimi/ingsw/gc07/fxml/lobbyScene.fxml"),
-    PLAYER_SCENE("Codex Naturalis - Board", "/it/polimi/ingsw/gc07/fxml/player.fxml"),
+    PLAYER_SCENE("Codex Naturalis - Board", "/it/polimi/ingsw/gc07/fxml/playerScene.fxml"),
     OTHER_PLAYER_SCENE("Codex Naturalis - Other board", "/it/polimi/ingsw/gc07/fxml/otherPlayerScene.fxml"),
     GAME_ENDED_SCENE("Codex Naturalis - Game ended", "/it/polimi/ingsw/gc07/fxml/gameEndedScene.fxml");
 
