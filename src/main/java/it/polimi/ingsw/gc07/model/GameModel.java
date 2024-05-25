10ee9f740ab9961aa768c5745f6f8b7d9ca1f979
SamueleVilla02 <samuele1.villa@mail.polimi.it>
2024-05-25 17:08:28 +0200

    added java doc
    @SV

--- src/main/java/it/polimi/ingsw/gc07/model/GameModel.java
+++ src/main/java/it/polimi/ingsw/gc07/model/GameModel.java
@@ -122,94 +122,178 @@ public class GameModel {
         this.emptyDecks = false;
     }
 
+    /**
+     * Getter method for attribute id.
+     * @return id of the game.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter method for attribute playersNumber.
+     * @return the number of players.
+     */
     public int getPlayersNumber() {
         return playersNumber;
     }
 
+    /**
+     * Setter method for attribute state.
+     * @param state state to be set.
+     */
     public void setState(GameState state) {
         this.state = state;
         // update listeners
         sendGameModelUpdate();
     }
 
+    /**
+     * Getter method for attribute state.
+     * @return state of the game.
+     */
     public GameState getState() {
         return state;
     }
 
+    /**
+     * Getter method for attribute players.
+     * @return the list of players.
+     */
     public List<Player> getPlayers() {
         return players;
     }
 
+    /**
+     * Getter method for nicknames of players.
+     * @return nicknames of players in the game.
+     */
     public List<String> getPlayerNicknames() {
         return players.stream().map(Player::getNickname).toList();
     }
 
+    /**
+     * Getter method for attribute winners.
+     * @return the list of winners.
+     */
     public List<String> getWinners() {
         return winners;
     }
 
+    /**
+     * Setter method for attribute currPlayer.
+     * @param currPlayer the current player that has to be set.
+     */
     public void setCurrPlayer(int currPlayer) {
         this.currPlayer = currPlayer;
         // update listeners
         sendGameModelUpdate();
     }
 
+    /**
+     * Getter method for attribute currPLayer.
+     * @return the current player.
+     */
     public int getCurrPlayer() {
         return currPlayer;
     }
 
+    /**
+     * Getter method for hasCurrPlayerPlaced attribute.
+     * @return if the current player has placed or not.
+     */
     public boolean getHasCurrPlayerPlaced() {
         return hasCurrPlayerPlaced;
     }
 
+    /**
+     * Setter method for attribute hasCurrPlayerPlaced.
+     * @param hasCurrPlayerPlaced the value that has to be set.
+     */
     public void setHasCurrPlayerPlaced(boolean hasCurrPlayerPlaced) {
         this.hasCurrPlayerPlaced = hasCurrPlayerPlaced;
     }
 
+    /**
+     * Getter method for attribute board.
+     * @return the board.
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * Getter method for attribute resourceCardsDeck.
+     * @return the resource card deck.
+     */
     public DrawableDeck<DrawableCard> getResourceCardsDeck() {
         return resourceCardsDeck;
     }
 
+    /**
+     * Getter method for attribute goldCardsDeck.
+     * @return the gold card deck.
+     */
     public DrawableDeck<GoldCard> getGoldCardsDeck() {
         return goldCardsDeck;
     }
 
+    /**
+     * Getter method for attribute objectiveCardsDeck.
+     * @return the objective card deck.
+     */
     public PlayingDeck<ObjectiveCard> getObjectiveCardsDeck() {
         return objectiveCardsDeck;
     }
 
+    /**
+     * Getter method for attribute starterCardsDeck.
+     * @return the starter card deck.
+     */
     public Deck<PlaceableCard> getStarterCardsDeck() {
         return starterCardsDeck;
     }
 
+    /**
+     * Getter method for attribute penultimateRound.
+     * @return if is the penultimate round or not.
+     */
     public boolean getPenultimateRound() {
         return penultimateRound;
     }
 
+    /**
+     * Setter method for attribute penultimateRound.
+     * @param penultimateRound the value that has to be set.
+     */
     public void setPenultimateRound(boolean penultimateRound) {
         this.penultimateRound = penultimateRound;
         // update listeners
         sendGameModelUpdate();
     }
 
+    /**
+     * Getter method for attribute additionalRound.
+     * @return if is the additional round or not.
+     */
     public boolean getAdditionalRound() {
         return additionalRound;
     }
 
+    /**
+     * Setter method for additionalRound.
+     * @param additionalRound the value that has to be set.
+     */
     public void setAdditionalRound(boolean additionalRound) {
         this.additionalRound = additionalRound;
         // update listeners
         sendGameModelUpdate();
     }
 
+    /**
+     * Method used to calculate the taken token colors.
+     * @return the taken token colors.
+     */
     public List<TokenColor> getTakenTokenColors() {
         List<TokenColor> takenColors = new ArrayList<>();
         for(Player p: players) {
@@ -218,6 +302,11 @@ public class GameModel {
         return takenColors;
     }
 
+    /**
+     * Setter method for attribute commandResult.
+     * @param nickname of the player that execute the command.
+     * @param commandResult the result that has to be set.
+     */
     public void setCommandResult(String nickname, CommandResult commandResult) {
         this.commandResult = commandResult;
 
@@ -232,18 +321,33 @@ public class GameModel {
         }
     }
 
+    /**
+     * Getter method for the attribute commandResult.
+     * @return the command result.
+     */
     public CommandResult getCommandResult() {
         return commandResult;
     }
 
+    /**
+     * Setter method for the attribute emptyDecks.
+     * @param value the boolean value that has to be set.
+     */
     public void setEmptyDecks(boolean value) {
         this.emptyDecks = value;
     }
 
+    /**
+     * Getter method for the attribute emptyDecks.
+     * @return if the decks are empty or not.
+     */
     public boolean getEmptyDecks() {
         return this.emptyDecks;
     }
 
+    /**
+     * Method used to send a game model update.
+     */
     private void sendGameModelUpdate() {
         GameModelUpdate update = new GameModelUpdate(id, state, currPlayer, penultimateRound, additionalRound);
         for(GameListener l: gameListeners) {
@@ -255,6 +359,9 @@ public class GameModel {
         }
     }
 
+    /**
+     * Method used to send a deck update.
+     */
     private void sendDeckUpdate() {
         DeckUpdate update = new DeckUpdate(resourceCardsDeck.revealTopCard(), goldCardsDeck.revealTopCard(),
                 resourceCardsDeck.getFaceUpCards(), goldCardsDeck.getFaceUpCards(),
@@ -268,6 +375,9 @@ public class GameModel {
         }
     }
 
+    /**
+     * Method used to send a winners update.
+     */
     private void sendWinnersUpdate() {
         for(GameListener l: gameListeners) {
             try {
@@ -278,12 +388,18 @@ public class GameModel {
         }
     }
 
+    /**
+     * Method used to set up the resource card deck.
+     */
     public void setUpResourceCardsDeck() {
         resourceCardsDeck.setUpDeck();
         // update listeners
         sendDeckUpdate();
     }
 
+    /**
+     * Method used to set up the gold card deck.
+     */
     public void setUpGoldCardsDeck() {
         goldCardsDeck.setUpDeck();
         // update listeners
@@ -291,12 +407,19 @@ public class GameModel {
 
     }
 
+    /**
+     * Method used to set up the objective card deck.
+     */
     public void setUpObjectiveCardsDeck() {
         objectiveCardsDeck.setUpDeck();
         // update listeners
         sendDeckUpdate();
     }
 
+    /**
+     * Method used to draw a card from the resource card deck.
+     * @return the drawn card.
+     */
     public DrawableCard drawResourceCard() {
         DrawableCard card = resourceCardsDeck.drawCard();
         // update listeners
@@ -304,6 +427,10 @@ public class GameModel {
         return card;
     }
 
+    /**
+     * Method used to draw a card from the gold card deck.
+     * @return the drawn card.
+     */
     public GoldCard drawGoldCard() {
         GoldCard card = goldCardsDeck.drawCard();
         // update listeners
@@ -311,6 +438,10 @@ public class GameModel {
         return card;
     }
 
+    /**
+     * Method used to draw a card from the objective card deck.
+     * @return the drawn card.
+     */
     public ObjectiveCard drawObjectiveCard() {
         ObjectiveCard card = objectiveCardsDeck.drawCard();
         // update listeners
@@ -318,6 +449,10 @@ public class GameModel {
         return card;
     }
 
+    /**
+     * Method used to draw a card from the starter card deck.
+     * @return the drawn card.
+     */
     public PlaceableCard drawStarterCard() {
         PlaceableCard card = starterCardsDeck.drawCard();
         // update listeners
@@ -325,6 +460,11 @@ public class GameModel {
         return card;
     }
 
+    /**
+     * Method used to draw a card from the face up resource card.
+     * @param pos the position from where the card has to be drawn.
+     * @return the drawn card.
+     */
     public DrawableCard drawFaceUpResourceCard(int pos) {
         DrawableCard card = resourceCardsDeck.drawFaceUpCard(pos);
         // update listeners
@@ -332,6 +472,11 @@ public class GameModel {
         return card;
     }
 
+    /**
+     * Method used to draw a card from the face up gold card.
+     * @param pos the position from where the card has to be drawn.
+     * @return the drawn card.
+     */
     public GoldCard drawFaceUpGoldCard(int pos) {
         GoldCard card = goldCardsDeck.drawFaceUpCard(pos);
         // update listeners
@@ -339,26 +484,48 @@ public class GameModel {
         return card;
     }
 
+    /**
+     * Method used to add a card to the face up resource card.
+     * @param card card that has to be added.
+     */
     public void addFaceUpResourceCard(DrawableCard card) {
         resourceCardsDeck.addFaceUpCard(card);
         // update listeners
         sendDeckUpdate();
     }
 
+    /**
+     * Method used to add a card to the face up gold card.
+     * @param card card that has to be added.
+     */
     public void addFaceUpGoldCard(GoldCard card) {
         goldCardsDeck.addFaceUpCard(card);
         // update listeners
         sendDeckUpdate();
     }
 
+    /**
+     * Method used to reveal a card from the face up resource card.
+     * @param pos position of the card that will be revealed.
+     * @return the card in the face up resource card.
+     */
     public DrawableCard revealFaceUpResourceCard(int pos) {
         return resourceCardsDeck.revealFaceUpCard(pos);
     }
 
+    /**
+     * Method used to reveal a card from the face up gold card.
+     * @param pos position of the card that will be revealed.
+     * @return the card in the face up gold card.
+     */
     public GoldCard revealFaceUpGoldCard(int pos) {
         return goldCardsDeck.revealFaceUpCard(pos);
     }
 
+    /**
+     * Method used to set up the player hand.
+     * @param newPlayer player whose hand will be set.
+     */
     public void setUpPlayerHand(Player newPlayer) {
         // set card hand
         newPlayer.addCardHand(drawResourceCard());
@@ -370,6 +537,10 @@ public class GameModel {
         newPlayer.setStarterCard(drawStarterCard());
     }
 
+    /**
+     * Method used to add a player to the game.
+     * @param newPlayer player that has to be added.
+     */
     public void addPlayer(Player newPlayer) {
         players.add(newPlayer);
         board.addPlayer(newPlayer.getNickname());
@@ -389,6 +560,10 @@ public class GameModel {
         // in order to update also the newly created listener
     }
 
+    /**
+     * Method used to add listener to the client.
+     * @param client client to which the listener has to be added.
+     */
     public void addListener(VirtualView client) {
         // called as soon as a player joins a game
         gameListeners.add(client);
@@ -422,6 +597,8 @@ public class GameModel {
 
     /**
      * Method used to send a full model view update to a client who has just reconnected.
+     * @param nickname nickname of the player to which the update will be sent.
+     * @param client client of the player to which the update will be sent.
      */
     public void sendModelViewUpdate(String nickname, VirtualView client) {
         // gameModel update and deckUpdate sent in addListener
@@ -459,6 +636,10 @@ public class GameModel {
         }
     }
 
+    /**
+     * Method used to remove a listener to a player.
+     * @param client client of the player to which the listener will be removed.
+     */
     public void removeListener(VirtualView client) {
         gameListeners.remove(client);
         chat.removeListener(client);
@@ -471,18 +652,38 @@ public class GameModel {
         System.out.println("Number of listeners: " + gameListeners.size());
     }
 
+    /**
+     * Method used to add a message to the public chat.
+     * @param content content of the message.
+     * @param sender nickname of the sender of the message.
+     */
     public void addChatPublicMessage(String content, String sender) {
         chat.addPublicMessage(content, sender);
     }
 
+    /**
+     * Method used to add a message to the private chat.
+     * @param content content of the message.
+     * @param sender nickname of the sender of the message.
+     * @param receiver nickname of the receiver of the message.
+     */
     public void addChatPrivateMessage(String content, String sender, String receiver) {
         chat.addPrivateMessage(content, sender, receiver);
     }
 
+    /**
+     * Method used to increment the score of a player.
+     * @param nickname nickname of the player to which points will be incremented.
+     * @param deltaScore value of the increment.
+     */
     private void incrementScore(String nickname, int deltaScore) {
         board.incrementScore(nickname, deltaScore);
     }
 
+    /**
+     * Method that return the number of players connected.
+     * @return the number of players connected.
+     */
     public int getNumPlayersConnected() {
         int numPlayersConnected = 0;
         for (Player p: players){
@@ -493,6 +694,11 @@ public class GameModel {
         return numPlayersConnected;
     }
 
+    /**
+     * Method that check if the game has a player or not.
+     * @param nickname nickname of the player that has to be checked in the game.
+     * @return if the game has the player or not.
+     */
     public boolean hasPlayer(String nickname) {
         for(Player p: players){
             if (p.getNickname().equals(nickname)) {
@@ -502,6 +708,11 @@ public class GameModel {
         return false;
     }
 
+    /**
+     * Method that check if a player has a specific token color.
+     * @param tokenColor token color that will be checked.
+     * @return if ha player has the specific token color or not.
+     */
     public boolean hasPlayerWithTokenColor(TokenColor tokenColor) {
         for(Player p: players){
             if(p.getTokenColor().equals(tokenColor)){
@@ -568,12 +779,22 @@ public class GameModel {
         sendWinnersUpdate();
     }
 
+    /**
+     * Setter method for attribute winners.
+     * @param nickname nickname of the winner that has to be added to the winners.
+     */
     public void setWinner(String nickname){
         winners.add(nickname);
         // update listeners
         sendWinnersUpdate();
     }
 
+    /**
+     * Method that add points to a player.
+     * @param player player to whom the points will be added.
+     * @param x row where the card is placed.
+     * @param y column where the card is placed.
+     */
     public void addPoints(Player player, int x, int y) {
         int deltaPoints;
         deltaPoints = player.getGameField().getPlacedCard(x, y).getPlacementScore(player.getGameField(), x, y);
