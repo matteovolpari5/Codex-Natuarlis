0f4107031b0f95c1fe213b79bf29b9854f34fbeb
SamueleVilla02 <samuele1.villa@mail.polimi.it>
2024-05-26 11:20:25 +0200

    added print the back of player hand
    @SV

--- src/main/java/it/polimi/ingsw/gc07/view/tui/Tui.java
+++ src/main/java/it/polimi/ingsw/gc07/view/tui/Tui.java
@@ -565,10 +565,14 @@ public class Tui implements Ui, ChatTui, DeckTui, GameFieldTui, PlayerTui, Board
         if(!(client.getGameView().getGameState().equals(GameState.GAME_STARTING) && (hand.size() < 3 || personalObjective == null))) {
             System.out.println();
             System.out.println("--------------------------------------------------------");
-            System.out.println("                      PLAYER HAND                       ");
+            System.out.println("                  FRONT PLAYER HAND                     ");
             System.out.println("--------------------------------------------------------");
             PlayerTui.printPlayerHand(hand, personalObjective,false);
-            //TODO: print also the back of the player hand
+            System.out.println();
+            System.out.println("--------------------------------------------------------");
+            System.out.println("                   BACK PLAYER HAND                     ");
+            System.out.println("--------------------------------------------------------");
+            PlayerTui.printPlayerHand(hand, personalObjective,true);
         }
     }
 
